e9a7ea692a447ad125bb638b479ba809ef681bae
roby <rob.young@adscale.co.nz>
2015-09-11 13:58:16 +1200

    Add pretty flower

---
+++ bash/greet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from termcolor import colored, cprint
+
+cprint('    _','yellow')
+cprint('  _( )_','yellow')
+cprint(' (_','yellow',end='')
+cprint('(%)','magenta',end='')
+cprint('_)','yellow')
+cprint('   (_)','yellow',end='')
+cprint('\\','green')
+cprint('       | __','green')
+cprint('       |/_/','green')
+cprint('       |','green')
+cprint('       |','green')
--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 function prompto {
-  python ./prompt.py
+  python ~/.dotfiles/bash/prompt.py
 }
 export PS1="\u@\h \w\[\e[0;32m\]\$(parse_git_branch)\[\e[0m\] \$(prompto)  "
