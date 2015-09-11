ebd9f1e5f22a22a0fef7bae2197b51285f748a7e
roby <rob.young@adscale.co.nz>
2015-09-11 13:53:38 +1200

    Add emoji prompt

---
+++ bash/prompt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+x = [u'♤',u'♡',u'♢',u'♧']
+print(random.choice(x).encode('utf-8'))
---
+++ bash/prompt.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+function prompto {
+  python ./prompt.py
+}
+export PS1="\u@\h \w\[\e[0;32m\]\$(parse_git_branch)\[\e[0m\] \$(prompto)  "
